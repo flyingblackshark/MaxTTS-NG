b482ee67b5c968418f991366dcd02e01a5429457
FBS <aliu2000@outlook.com>
2025-02-02 22:54:33 +0800

    test

--- MaxText/hf_dac_encode_mls_eng_dataset.py
+++ MaxText/hf_dac_encode_mls_eng_dataset.py
@@ -14,12 +14,12 @@ import os
 import tensorflow as tf
 from array_record.python.array_record_module import ArrayRecordWriter
 import tiktoken
-from datasets import disable_caching
+#from datasets import disable_caching
 from collections import defaultdict
 from jax.experimental.compilation_cache import compilation_cache as cc
 cc.set_cache_dir("/tmp/jax_cache")
-disable_caching()
-os.environ["HF_DATASETS_IN_MEMORY_MAX_SIZE"]=str(1024*1024*1024*64)
+#disable_caching()
+#os.environ["HF_DATASETS_IN_MEMORY_MAX_SIZE"]=str(1024*1024*1024*64)
 
 DEVICE = "tpu"
 MAX_LENGTH_AUDIO = 30 * 44100
