ab20b610ecbbb550079f5037d6d1b6b38731f5da
FBS <aliu2000@outlook.com>
2025-02-02 14:25:48 +0800

    test

--- MaxText/hf_dac_encode_mls_eng_dataset.py
+++ MaxText/hf_dac_encode_mls_eng_dataset.py
@@ -142,11 +142,12 @@ if __name__ == "__main__":
 
     for item in multihost_gen:
         print(f"round {i}")
-        if i%10240 == 0:
-            num = i//10240
-            if writer is not None:
-                writer.close() 
-            writer = ArrayRecordWriter(f"/home/blacksharkflying/bucket/dac_dataset_1/mls_eng_train_part_{num}.arrayrecord", 'group_size:1')
+        if jax.process_index() == 0:
+            if i%10240 == 0:
+                num = i//10240
+                if writer is not None:
+                    writer.close() 
+                writer = ArrayRecordWriter(f"/home/blacksharkflying/bucket/dac_dataset_1/mls_eng_train_part_{num}.arrayrecord", 'group_size:1')
             
         semantics = encode_to_codes(jnp.expand_dims(item["audio"],1))
         i+=1
