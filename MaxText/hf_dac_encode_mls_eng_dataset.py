12c489f6ea3ab94b1ad7c011b15fca57f4c30dda
FBS <aliu2000@outlook.com>
2025-02-25 14:59:00 +0800

    test

--- MaxText/hf_dac_encode_mls_eng_dataset.py
+++ MaxText/hf_dac_encode_mls_eng_dataset.py
@@ -95,21 +95,16 @@ if __name__ == "__main__":
         
         return {'input_ids': ids}
     dataset = dataset.map(process)
-    def resample_audio(example):
+    def resample_audio(batch):
         # 加载音频，保持原采样率
-        audio, sr = librosa.load(io.BytesIO(example["audio"]["bytes"]), sr=44100)  # 假设原始采样率为 16kHz
-        
-        # 重采样到 44.1kHz
-        #audio_resampled = librosa.resample(audio, sr, 44100)
-        
-        # 返回更新后的音频和采样率
-        example['audio'] = audio
-        example['sampling_rate'] = 44100  # 更新为新采样率
-        
-        return example
+        with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
+            resampled_audio = list(executor.map(lambda audio: librosa.load(io.BytesIO(audio["bytes"]), sr=44100)[0], batch["audio"]))
+            #audio, sr = librosa.load(io.BytesIO(example["audio"]["bytes"]), sr=44100)  # 假设原始采样率为 16kHz
+        batch["audio"] = resampled_audio
+        return batch
 
     # 使用 map 函数批量处理数据集
-    dataset = dataset.map(resample_audio,num_proc=4)
+    dataset = dataset.map(resample_audio, batched=True, batch_size=128)
 
     def get_sharding_for_spec(pspec: PartitionSpec) -> NamedSharding:
         """
